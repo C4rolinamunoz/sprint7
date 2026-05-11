8d2b477c2982b28bf79f8f918d983e7f053a9a54
C4rolinamunoz <Carolinamunozrico@gmail.com>
2026-05-11 13:02:56 -0500

    nuevos cambios

--- app.py
+++ app.py
@@ -6,21 +6,34 @@ import streamlit as st
 import plotly.express as px
 
 
- # leer los datos
+
 car_data = pd.read_csv('vehicles_us.csv')
 
-fig = px.histogram(car_data, x="price")
-fig2 = px.scatter(car_data, x="condition", y="price", color="model")
+grafica = px.histogram(car_data, x="price")
+grafica2 = px.scatter(car_data, x="condition", y="price", color="model")
+grafica3 = px.scatter(car_data, x="odometer", y="price") 
 
 
-st.write('DataSet para analizar el conjunto de datos de anuncios de venta de coches') 
+st.header('DataSet para analizar el conjunto de datos de anuncios de venta de coches') 
 
     
 st.write(car_data) 
-         
-         
-st.write('DataSet analizado por precio') 
-st.plotly_chart(fig, use_container_width=True)
 
-st.write('DataSet analizado por condición y precio')
-st.plotly_chart(fig2, use_container_width=True)
+
+showData =  st.button('Analizar el DataSet')
+
+
+if showData:
+    st.write('DataSet analizado por precio') 
+    st.plotly_chart(grafica, use_container_width=True)
+
+    st.write('DataSet analizado por condición y precio')
+    st.plotly_chart(grafica2, use_container_width=True)
+
+    st.write('DataSet analizado por dispersion, teniendo en cuenta odometer y precio')
+    st.plotly_chart(grafica3, use_container_width=True)
+
+
+         
+         
+
